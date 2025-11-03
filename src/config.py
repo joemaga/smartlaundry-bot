de6d2f2575367d2d6140696a93b14353799f4066
Joseph Magalhaes <joemaga10@gmail.com>
2025-11-03 16:59:02 -0300

    Add Twilio environment variables to config

--- src/config.py
+++ src/config.py
@@ -6,9 +6,12 @@ load_dotenv()
 class Config:
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     COMPANY_NAME = os.getenv('COMPANY_NAME', 'SmartLaundry Alphaville')
+    TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+    TWILIO_WHATSAPP_NUMBER = os.getenv('TWILIO_WHATSAPP_NUMBER')
     
     DEFAULT_PARAMETERS = {
-        "response_tone": "amigável e profissional",
+        "response_tone": "amigável e profissional", 
         "max_response_length": 300,
         "language": "português brasileiro"
     }
